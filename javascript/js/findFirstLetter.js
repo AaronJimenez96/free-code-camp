4fb204bff226dcaf0cd09237a09d34f7d6555643
Aaron Jimenez <ajimenez@ficachi.com.mx>
2021-11-22 16:04:53 -0600

    strings

--- javascript/js/findFirstLetter.js
+++ javascript/js/findFirstLetter.js
@@ -1,5 +1,6 @@
+//Remember that you can't modifi const variables
 /*How many character have a string*/
-const myName= "Aaron";
+var myName= "Aaron"; 
 let myNameLength = myName.length;
 console.log(myNameLength);
 
@@ -12,7 +13,18 @@ let findSome = myName[3];
 console.log(findSome);
 
 /*Concat variable operatar plus*/
+const saySomething = "Hi "+ myName + " is good see you again!";
+console.log(saySomething);
 
 /*Concat string with plus operator*/
 const myStr = "This is the start. " + "This is the end.";0
 console.log(myStr);
+
+/*Contact string with equals operator*/
+myName += " Jimenez Cortes";
+console.log(myName);
+
+/*Use Bracket Notation to Find the Nth-to-Last Character in a StringPassed */
+const lastName = "Lovelace";
+const secondToLastLetterOfLastName = lastName[lastName.length -2]; 
+console.log(secondToLastLetterOfLastName);
