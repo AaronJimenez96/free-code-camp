b99f11f875bf8c2fdbd1c0fc6e160c49419c87f8
Aaron Jimenez <ajimenez@ficachi.com.mx>
2021-11-22 11:50:56 -0600

    js examples strings

---
+++ javascript/findFirstLetter.js
@@ -0,0 +1,18 @@
+/*How many character have a string*/
+const myName= "Aaron";
+let myNameLength = myName.length;
+console.log(myNameLength);
+
+/*Find the first character*/
+let findFirstCharacter = myName[0];
+console.log(findFirstCharacter);
+
+/*Find the letter O*/
+let findSome = myName[3];
+console.log(findSome);
+
+/*Concat variable operatar plus*/
+
+/*Concat string with plus operator*/
+const myStr = "This is the start. " + "This is the end.";0
+console.log(myStr);
