0e46de6d2e13cd47493bd1a5e9d1d82f2aab2465
Aaron Jimenez <ajimenez@ficachi.com.mx>
2022-07-23 14:02:03 -0500

    react projects

--- javascript/js/findFirstLetter.js
+++ javascript/js/findFirstLetter.js
@@ -1,30 +1,14 @@
-//Remember that you can't modifi const variables
-/*How many character have a string*/
-var myName= "Aaron"; 
-let myNameLength = myName.length;
-console.log(myNameLength);
+function validate(){
+	let formInputs = Array.prototype.slice.call(document.querySelectorAll('input'));
 
-/*Find the first character*/
-let findFirstCharacter = myName[0];
-console.log(findFirstCharacter);
-
-/*Find the letter O*/
-let findSome = myName[3];
-console.log(findSome);
-
-/*Concat variable operatar plus*/
-const saySomething = "Hi "+ myName + " is good see you again!";
-console.log(saySomething);
-
-/*Concat string with plus operator*/
-const myStr = "This is the start. " + "This is the end.";0
-console.log(myStr);
-
-/*Contact string with equals operator*/
-myName += " Jimenez Cortes";
-console.log(myName);
-
-/*Use Bracket Notation to Find the Nth-to-Last Character in a StringPassed */
-const lastName = "Lovelace";
-const secondToLastLetterOfLastName = lastName[lastName.length -2]; 
-console.log(secondToLastLetterOfLastName);
+	formInputs.forEach((valInput) => {
+			if(valInput.classList.contains('val-email')){
+				extraEval(valInput,0) == true?
+						valInput.classList.remove('borde-invalid'):valInput.classList.add('borde-invalid');
+			}
+			else if(valInput.parentElement.classList.contains('val-tel') || valInput.classList.contains('val-tel')){		
+					extraEval(valInput,1) == true?
+							valInput.classList.remove('borde-invalid'):valInput.classList.add('borde-invalid');
+			}
+	});	
+}
